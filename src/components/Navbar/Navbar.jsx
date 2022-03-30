f2358c60af1fc4d1f6e3ee8af7e7165748384595
Tanishka-dev <tanishakayadav2110@gmail.comm>
2022-03-31 03:17:36 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./Navbar.scss";
+import { images } from "../../constants";
 function Navbar() {
-   return <div>Navbar</div>;
+   return (
+      <nav className="app__navbar">
+         <div className="app__navbar-logo">
+            <img src={images.logo} alt="logo" />
+         </div>
+         <ul className="app__navbar-links">
+            {["home", "about", "contact", "skills", "work"].map((item) => (
+               <li key={`link-${item}`} className="app__flex p-text">
+                  <div />
+                  <a href={`#${item}`}>{item}</a>
+               </li>
+            ))}
+         </ul>
+      </nav>
+   );
 }
 
 export default Navbar;
